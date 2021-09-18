0cf23dcb9a138f6f1bd090f5039795a05bf419b2
Jojo974 <70891489+Jojo974@users.noreply.github.com>
2021-09-18 12:26:13 +0400

    Update scripts.js
    MAJ 1.0

--- scripts.js
+++ scripts.js
@@ -121,3 +121,54 @@ function loader(){
     // }, '-=0.8')
 
 }
+
+
+
+// Contact Form
+
+
+window.addEventListener("DOMContentLoaded", function () {
+  // get the form elements defined in your form HTML above
+
+  var form = document.getElementById("my-form");
+  // var button = document.getElementById("my-form-button");
+  var status = document.getElementById("status");
+
+  // Success and Error functions for after the form is submitted
+
+  function success() {
+    form.reset();
+    status.classList.add("success");
+    status.innerHTML = "Votre message a bien été envoyé !";
+  }
+
+  function error() {
+    status.classList.add("error");
+    status.innerHTML = "Oops! Un problème est survenu.";
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
